edb7c398eb651937bb47a72f33b4d7d81b419ad3
Stefan Wierda <S.Wierda@fugro.nl>
2019-07-05 14:45:11 +0200

    Include cmake.exe

--- build.ps1
+++ build.ps1
@@ -1,10 +1,7 @@
 Write-Host "Fugro.OpenCV build script"
 
-if(!(Test-Path -Path 'bld'))
-{
-	New-Item -Path 'bld' -ItemType Directory
-}
 
-$command = '.\tools\bin\cmake.exe -G "Visual Studio 15 2017 Win64" -S ./source -B ./bld'
+
+$command = './tools/bin/cmake.exe -G "Visual Studio 15 2017 Win64" -S ./source -B ./bld'
 iex $command
 
